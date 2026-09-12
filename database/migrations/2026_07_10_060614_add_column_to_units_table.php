1ed5970e26fd6a7f4573abce4d7e0b0700844dad
USMAN_SY <blackdev504@gmail.com>
2026-09-12 23:12:10 +0100

    migration updt

--- database/migrations/2026_07_10_060614_add_column_to_units_table.php
+++ database/migrations/2026_07_10_060614_add_column_to_units_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('units', function (Blueprint $table) {
-//            $table->unsignedTinyInteger('order')->after('slug');
+            $table->unsignedTinyInteger('order')->after('slug');
         });
     }
 
