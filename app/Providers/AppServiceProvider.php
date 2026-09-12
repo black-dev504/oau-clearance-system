488a3c3493ed7bfe56dfb505c7ce59b3a6822026
USMAN_SY <blackdev504@gmail.com>
2026-09-13 00:08:17 +0100

    migration updt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         UploadedFile::macro('storeOnCloudinary', function (string $folder = 'student_clearance_pics') {
             return Cloudinary::uploadApi()->upload(
                 $this->getRealPath(),
