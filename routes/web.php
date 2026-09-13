d236455cfe4283b7a3744cedebcd81b72bde722e
USMAN_SY <blackdev504@gmail.com>
2026-09-13 20:11:26 +0100

    route updt

--- routes/web.php
+++ routes/web.php
@@ -23,22 +23,21 @@ Route::get('login', Login::class)->name('login');
 Route::post('logout', Logout::class)->name('logout');
 
 
-try {
-    if (Schema::hasTable('units')) {
-        $units = Unit::pluck('slug')->toArray();
 
-        foreach ($units as $unit) {
-            Route::prefix($unit)->name($unit . '.')->middleware(['auth', 'role:officer'])->group(function () {
-                Route::get('dashboard', OfficerDashboard::class)->name('dashboard');
-                Route::get('clearance-requests', ClearanceRequests::class)->name('clearance-requests');
-                Route::get('announcements', Announcements::class)->name('announcements');
-                Route::get('emails', Emails::class)->name('emails');
-            });
-        }
+if (Schema::hasTable('units')) {
+    $units = Unit::pluck('slug')->toArray();
+
+    foreach ($units as $unit) {
+        Route::prefix($unit)->name($unit . '.')->middleware(['auth', 'role:officer'])->group(function () {
+            Route::get('dashboard', OfficerDashboard::class)->name('dashboard');
+            Route::get('clearance-requests', ClearanceRequests::class)->name('clearance-requests');
+            Route::get('announcements', Announcements::class)->name('announcements');
+            Route::get('emails', Emails::class)->name('emails');
+        });
     }
-} catch (\Illuminate\Database\QueryException $e) {
-    logger()->error($e);
 }
+
+Route::get('student/dashboard', Student::class)->name('student.dashboard')->middleware(['auth','role:student']);
 
 Route::prefix(config('app.admin_prefix'))->name('admin.')->middleware(['auth', 'role:admin'])->group(function () {
     Route::get('dashboard', AdminDashboard::class)->name('dashboard');
