d2bec6eae562832c059c5a8b01a5339dbe27e08d
USMAN_SY <blackdev504@gmail.com>
2026-09-13 18:13:47 +0100

    prod url scheme updt

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'auth' => Authenticate::class,
         ]);
     })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
